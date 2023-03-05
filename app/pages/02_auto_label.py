58a437c89f85afe095ed9a5af543e8b90dfabc2d
miguelvalente <miguelvalente@protonmail.com>
2023-03-05 13:42:28 +0100

    description

--- app/pages/02_auto_label.py
+++ app/pages/02_auto_label.py
@@ -7,3 +7,15 @@ st.set_page_config(
     layout="centered",
     initial_sidebar_state="expanded",
 )
+
+st.markdown(
+    """
+            # Auto-Label 🤖
+
+            If you diarized your files you can use this page to
+            automatically find the same speakers across the diarizations
+
+            #NOTE: This feature is highly dependent on the quality of the diarization
+            and the quality of the audio files. Uniformity of the audio files is paramount
+    """
+)
