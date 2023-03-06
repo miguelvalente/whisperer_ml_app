b0ee290058d15f900321918ff55466cd6917e105
miguelvalente <miguelvalente@protonmail.com>
2023-03-06 15:52:41 +0100

    expanded auto-label sidebar

--- app/pages/02_auto_label.py
+++ app/pages/02_auto_label.py
@@ -7,7 +7,7 @@ st.set_page_config(
     page_title="Auto-Label",
     page_icon="🤖",
     layout="centered",
-    initial_sidebar_state="collapsed",
+    initial_sidebar_state="expanded",
 )
 
 st.markdown(
